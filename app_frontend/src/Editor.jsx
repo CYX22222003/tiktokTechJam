791416adbf8f27c6382c2aa139e6b74194df0a6d
guest1@CHENYIXUN <you@example.com>
2024-07-05 23:49:45 +0800

    Add Editor in the frontend

---
+++ app_frontend/src/Editor.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+import StarterKit from "@tiptap/starter-kit";
+import {
+  MenuButtonBold,
+  MenuButtonItalic,
+  MenuControlsContainer,
+  MenuDivider,
+  MenuSelectHeading,
+  RichTextEditor
+} from "mui-tiptap";
+import { useRef } from "react";
+
+export default function Editor() {
+  const rteRef = useRef(null);
+
+  return (
+    <div>
+      <RichTextEditor
+        ref={rteRef}
+        extensions={[StarterKit]} // Or any Tiptap extensions you wish!
+        content="<p>Hello world</p>" // Initial content for the editor
+        // Optionally include `renderControls` for a menu-bar atop the editor:
+        renderControls={() => (
+          <MenuControlsContainer>
+            <MenuSelectHeading />
+            <MenuDivider />
+            <MenuButtonBold />
+            <MenuButtonItalic />
+            {/* Add more controls of your choosing here */}
+          </MenuControlsContainer>
+        )}
+      />
+
+      <Button onClick={() => console.log(rteRef.current?.editor?.getHTML())}>
+        Log HTML
+      </Button>
+    </div>
+  );
+}
